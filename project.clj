09a788dbd869a21514ac282cc8ae0f4d0943ad42
Simon Brooke <simon@journeyman.cc>
2018-07-19 22:50:42 +0100

    /1.4.2:

--- project.clj
+++ project.clj
@@ -1,25 +1,37 @@
 (defproject adl "1.4.2-SNAPSHOT"
-  :description "An application to transform an ADL application specification document into skeleton code for a Clojure web-app"
-  :url "http://example.com/FIXME"
+  :description "An application to transform an ADL application specification
+  document into skeleton code for a Clojure web-app"
+  :url "https://github.com/simon-brooke/adl"
   :license {:name "GNU General Public License,version 2.0 or (at your option) any later version"
             :url "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html"}
-  :dependencies [[adl-support "0.1.0-SNAPSHOT"]
+
+  :dependencies [[adl-support "0.1.2"]
+                 [bouncer "1.0.1"]
+                 [clojure-saxon "0.9.4"]
+                 [environ "1.1.0"]
+                 [hiccup "1.0.5"]
                  [org.clojure/clojure "1.8.0"]
                  [org.clojure/math.combinatorics "0.1.4"]
-                 [org.clojure/tools.cli "0.3.7"]
-                 [bouncer "1.0.1"]
-                 [environ "1.1.0"]
-                 [hiccup "1.0.5"]]
+                 [org.clojure/tools.cli "0.3.7"]]
+
   :aot  [adl.main]
+
   :main adl.main
+
   :plugins [[lein-codox "0.10.3"]
             [lein-release "1.0.5"]]
 
+  ;; `lein release` doesn't work with `git flow release`. To use
+  ;; `lein release`, first merge `develop` into `master`, and then, in branch
+  ;; `master`, run `lein release`
+
   :release-tasks [["vcs" "assert-committed"]
+                  ["clean"]
+                  ["test"]
+                  ["codox"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
                   ;; ["vcs" "tag"] -- not working, problems with secret key
-                  ["clean"]
                   ["uberjar"]
                   ["change" "version" "leiningen.release/bump-version"]
                   ["vcs" "commit"]])
