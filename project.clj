ef7c13fadc7369f89c62d207f5683120ae2f90a1
Simon Brooke <simon@journeyman.cc>
2018-09-20 11:23:35 +0100

    Updated release documentation comment.

--- project.clj
+++ project.clj
@@ -24,9 +24,8 @@
             ;; [uncomplexor "0.1.0-SNAPSHOT"]
             ]
 
-  ;; `lein release` doesn't work with `git flow release`. To use
-  ;; `lein release`, first merge `develop` into `master`, and then, in branch
-  ;; `master`, run `lein release`
+  ;; `lein release` doesn't play nice with `git flow release`. Run `lein release` in the
+  ;; `develop` branch, then reset the `master` branch to the release tag.
 
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
